5ce718d960b60b1c7ba8b37dc2d37ee1383a4c1e
Raj Nathani <relfor@outlook.com>
2012-12-06 00:14:56 -0500

    fixed comment imabalance at config variables

--- powerhouse.js
+++ powerhouse.js
@@ -1,17 +1,17 @@
 "use strict"
 
 /*
-/*Config Variables*/
+//Config Variables
 
 var popID = ?;
 var backID = ?;
 
-/*Optional (for animations (on popup cancel)*/ 
+//Optional (for animations (on popup cancel) 
 var animation_out_length = ?; //duration in milliseconds;
 var back_animation_out_class_name = ?;
 var popup_animation_out_class_name = ?;
 
-/*for ajax*/
+//for ajax
 var $SCRIPT_ROOT = ?;
 */
 
