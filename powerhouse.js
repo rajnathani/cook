76dbd7616439978fd9a8e9462161c4da9ed6f9f2
relfor <relfor@outlook.com>
2013-03-04 00:17:29 -0500

    removes popups and dependency of jQuery.
    - removed popups: popups is now a part of another project by name of
    popup available at github.com/relfor/popup
    - removed dependecy of jQuery: powerhouse is now independent of jQuery
    - reintroduced add_class, remove_class, has_class which had been
    previously depracated

--- powerhouse.js
+++ powerhouse.js
@@ -1,19 +1,5 @@
 "use strict"
 
-/*
-//Config Variables
-
-var popID = ?;
-var backID = ?;
-
-//Optional (for animations (on popup cancel) 
-var animation_out_length = ?; //duration in milliseconds;
-var back_animation_out_class_name = ?;
-var popup_animation_out_class_name = ?;
-
-//for ajax
-var $SCRIPT_ROOT = ?;
-*/
 
 
 function get(to_get){
@@ -27,25 +13,40 @@ function get(to_get){
 
 function create_element(tag, element_build_dict){
 	var created_node = document.createElement(tag);
-	
+
     if (element_build_dict != undefined){
-	$.each( element_build_dict, function( key, value ) {
+        var key,value;
+    for (key in element_build_dict) {
+        value = element_build_dict[key];
 		if (key === "Structure") { created_node.appendChild(value);}
 		else if (key === "ID") { created_node.id = value;}
-		else if (key === "Class") { $(created_node).addClass(value);}
+		else if (key === "Class") {add_class(created_node, value);}
 		else if (key === "Classes") {
 			for(var i=0; i < value.length; i++){
-			$(created_node).addClass(value[i]);
+			add_class(created_node(value[i]));
 			}
 		} else if (key === "Text") { add_text(created_node,value);}
 		else{
-			$(this).attr(key, value);
+			this.setAttribute(key, value);
 		}
-    });
+    }
     }
 	return created_node;
 }
 
+function add_class(node, class_name){
+    	node.className += (" " + class_name);
+}
+
+function remove_class(node, class_name){
+        	node.className = node.className.replace(class_name, '');
+        	node.className = node.className.replace((' ' + class_name), '');
+        	node.className = node.className.replace((class_name + ' '), '');
+}
+
+function has_class(node, class_name){
+    	return (node.className.indexOf(class_name) != - 1)
+    }
 
 function add_text(node, text){
 	node.appendChild(document.createTextNode(text));
@@ -91,122 +92,17 @@ function toggle_display(node, display_type){
 	}
 }
 
-function toggle_focus(node){
-	if (is_there(node)){
-		if (document.activeElement == node) $(node).focus();
-		else  $(node).blur();
-	}
-}
-
-//Source: Mozilla Development Network
-function animationPossible(){
-
-	var animation = false;
-    var animationstring = 'animation';
-    var keyframeprefix = '';
-    var domPrefixes = 'Webkit O MS MOZ'.split(' ');
-    pfx  = '';
- 
-	if( elm.style.animationName ) { animation = true; }    
- 
-	if( animation === false ) {
-	  for( var i = 0; i < domPrefixes.length; i++ ) {
-	    if( elm.style[ domPrefixes[i] + 'AnimationName' ] !== undefined ) {
-	      var pfx = domPrefixes[ i ];
-	      animationstring = pfx + 'Animation';
-	      keyframeprefix = '-' + pfx.toLowerCase() + '-';
-	      animation = true;
-	      break;
-	    }
-	  }
-	}
-	
-	return animation;
-}
-
-function pop_up(content, extra_class ){
-    if (get("#" + backID)) {return 0;}
-      
-    var backy = create_element('div');
-    backy.id = backID;
-    backy.style.height = get_doc_height() + "px";
-
- 
-    var popy = create_element('div');
-    popy.id = popID;
-    if (is_there(extra_class)){
-    	popy.className = extra_class;
-    }
-    popy.style.marginTop = ((get_inner_height()/3.45 + get_scrolled()) + "px");
-	
-
-    var cancel = create_element('span');
-    cancel.id = 'cancel';
-    cancel.innerHTML = 'x';
-
-    cancel.onclick = function(){
-		
-    	pop_out();
-    };
-	
-	if (is_there(content.substring)){
-    	var content_area = create_element('p', content);
-    }else{
-    	var content_area = content;
-    }
-
-    popy.appendChild(cancel);
-    popy.appendChild(content_area);
-    
-    
-    backy.appendChild(popy);
-   
-	
-	
-    document.body.appendChild(backy);
-    
-	document.onkeydown = function(){
-		if (get_key_code(window.event) == 27){
-			pop_out();
-		}
-	}
-	
-}
-
-function pop_out(){
-	document.onkeydown = function(){}
-	animate_out(animation_out_length, backID,{backID: back_animation_out_class_name, popID: popup_animation_out_class_name });
-}
-
-
 function animate_out(delay_length, main, todo){
-	var delay = 0;
-	
-	if (animationPossible){
-		delay = delay_length - 30;
-	}
-	setTimeout(function(){$('#' + main).remove(); }, delay);
-	$.each(todo, function(key, value) { 
-  		$(key).addClass(value);
-	});
-}
+    var delay = 0;
 
-
-function go_ajax(url, method, data, success_func){
-    if (!is_there(success_func)){
-        success_func = function(){location.reload();}
+    if (animationPossible){
+        delay = delay_length - 30;
     }
-
-    $.ajax({
-        url:$SCRIPT_ROOT + url,
-        type:method,
-        data:  JSON.stringify(data),
-        contentType:"application/json;charset=UTF-8",
-        success: success_func,
-        error: function(){alert('something went wrong');}
-    });
+    setTimeout(function(){get('#' + main).remove(); }, delay);
+    for (var key in todo){
+        var value = todo[key];
+        add_class(key,value);
+    }
 }
 
-function bring_json(url, data, func){
-    $.getJSON($SCRIPT_ROOT + url, data, func);
-}
+
